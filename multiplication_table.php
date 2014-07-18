501e489071abc550e3751820ab036697a466ec3c
carrie-pan <i7807@yahoo.com.tw>
2014-07-18 13:32:47 +0800

    99乘法表

---
+++ multiplication_table.php
@@ -0,0 +1,35 @@
+﻿<html>
+<head>
+	<style>
+		table
+		{ 
+		  border: 1px; 
+		  font-family: arial; 
+		  font-size:14px; 
+		  border-style:ridge;
+		} 
+		td
+		{ 
+		  border-bottom:1px solid #333; 
+		  border-right:1px solid #333; 
+		} 
+
+	</style>
+</head>
+	<body>
+		<table>
+			<caption>99乘法表</caption>
+		<?php
+		for($i=1;$i<=9;$i++)
+		{
+			echo '<tr>';
+			for($n=1;$n<=9;$n++)
+			{
+				echo '<td>'.$i.'*'.$n.'='.($i*$n).'</td>';
+			}
+			echo '</tr>';
+		}
+		?>
+		</table>
+	</body>
+</html>
